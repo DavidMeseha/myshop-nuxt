80d7613e932b8ac4d70bc157c801debbfbe72b14
David <davidmmyh@gmail.com>
2025-03-30 22:05:44 +0200

    Loading indecator

--- src/app.vue
+++ src/app.vue
@@ -1,6 +1,7 @@
 <template>
   <Html :lang="htmlAttrs.lang" :dir="htmlAttrs.dir">
     <Body>
+      <NuxtLoadingIndicator color="#3A3DF0" :height="6" class="z-50" />
       <NuxtLayout>
         <NuxtPage />
       </NuxtLayout>
